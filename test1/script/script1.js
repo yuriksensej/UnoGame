6180eda140b737fe7872cf132eb8655dca5e738e
yuriksensej <yuraminchenko@gmail.com>
2021-02-27 20:11:44 +0200

    UnoNotes v1

--- test1/script/script1.js
+++
@@ -1,9 +0,0 @@
-let arr = [];
-arr['Hello'] = 'Yurii';
-arr[0] = 'Min';
-arr[1] = 'chenko';
-for (let i = 0; i < arr.length; i++) {
-  const element = arr[i];
-  console.log(element);
-}
-console.log(arr);
